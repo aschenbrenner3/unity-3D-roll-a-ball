cee64b3f90776750f00b777d463052a8e8bcb54f
Stephen Phillips <stephenphillips@Stephens-MBP.home>
2018-02-22 10:39:26 -0500

    initial commit Assets and Project Settings folders

---
+++ Assets/Scripts/pickup_rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup_rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame - use over FixedUpdate when not using forces
+	void Update () {
+		transform.Rotate ( new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/player_controller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_controller : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText; 
+	private Rigidbody rb;
+	private int count;
+
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	public void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVeritcal = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVeritcal);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+			count = count + 1; 
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 2) { 
+			winText.text = "Winner!";
+		}
+	}
+
+	void End(){
+		
+	}
+}
+		
+
